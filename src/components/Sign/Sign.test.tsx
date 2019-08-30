8963ad60b7b3311c30b1817dfb16abd0d68711c7
Akin Aguda <lordtitan7@gmail.com>
2019-08-30 18:13:40 +0100

    setup testing tools

---
+++ src/components/Sign/Sign.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Sign from '.';
+import reducer from '../../reducer';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { SignType } from '../../interfaces';
+configure({ adapter: new Adapter() });
+
+describe('<Sign />', () => {
+  const store = createStore(reducer);
+  it('should render correctly', () => {
+    // tslint:disable-next-line: no-angle-bracket-type-assertion
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Sign />
+      </Provider>,
+    );
+  });
+});
