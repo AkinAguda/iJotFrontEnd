491a09c101eda5f05d3dac40e9575bde14409d64
Akin Aguda <lordtitan7@gmail.com>
2019-09-12 16:15:36 +0100

    fixed routing problem

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import firebase from '../../Firebase';
 import { LOG_USER_IN } from '../../reducer/actions';
 import { useDispatch, useSelector } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import { SignType, ProtectedRouteType, UserStates } from '../../interfaces';
 import Sign from '../Sign';
 import Shell from '../Shell';
@@ -11,67 +11,74 @@ import Loading from '../elements/Loading';
 const Routes: React.FC = (): JSX.Element => {
   const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state: UserStates) => state);
-  const [isLoaded, setLoaded] = useState(true);
-  // useEffect(() => {
-  //   firebase.auth().onAuthStateChanged((user: any) => {
-  //     if (user) {
-  //       dispatch({ type: LOG_USER_IN });
-  //     }
-  //     setLoaded(true);
-  //   });
-  // }, []);
-
-  // const ProtectedRoute = ({
-  //   path,
-  //   component: Component,
-  //   auth,
-  //   exact,
-  //   to,
-  //   ...props
-  // }: ProtectedRouteType): React.ReactElement =>
-  //   auth ? (
-  //     <Route
-  //       path={path}
-  //       exact={!!exact}
-  //       render={(): any => <Component {...props} />}
-  //     />
-  //   ) : (
-  //     <Redirect to={to || '/signin'} />
-  //   );
+  const [isLoaded, setLoaded] = useState(false);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user: any) => {
+      if (user) {
+        dispatch({ type: LOG_USER_IN });
+      }
+      setLoaded(true);
+    });
+  }, []);
 
   return isLoaded ? (
     <>
-      {/* <Route
-        path="/"
-        exact={true}
-        render={(props: SignType): React.ReactElement => (
-          <Sign {...props} signUp={true} />
-        )}
-      />
-      <Route
-        path="/signin"
-        exact={true}
-        render={(props: SignType): React.ReactElement => <Sign {...props} />}
-      />
-      <ProtectedRoute
-        path="/notes"
-        auth={isLoggedIn}
-        exact={true}
-        component={Shell}
-        notes={true}
-      />
-      <ProtectedRoute
-        path="/edit"
-        auth={isLoggedIn}
-        exact={true}
-        component={Shell}
-        edit={true}
-      /> */}
-      <Route
-        path="/edit"
-        exact={true}
-        render={(props): React.ReactElement => <Shell {...props} edit={true} />}
-      />
+      {!isLoggedIn ? (
+        <>
+          <Switch>
+            <Route
+              path="/signin"
+              exact={true}
+              render={(props: SignType): React.ReactElement => (
+                <Sign {...props} />
+              )}
+            />
+            <Route
+              path="/"
+              exact={true}
+              render={(props: SignType): React.ReactElement => (
+                <Sign {...props} signUp={true} />
+              )}
+            />
+            <Redirect to="/" />
+          </Switch>
+        </>
+      ) : (
+        <>
+          <Switch>
+            <Route
+              path="/notes"
+              exact={true}
+              render={(props): React.ReactElement => (
+                <Shell {...props} notes={true} />
+              )}
+            />
+            <Route
+              path="/edit"
+              exact={true}
+              render={(props): React.ReactElement => (
+                <Shell {...props} edit={true} />
+              )}
+            />
+
+            <Route
+              path="/signin"
+              exact={true}
+              render={(props: SignType): React.ReactElement => (
+                <Sign {...props} />
+              )}
+            />
+            <Route
+              path="/"
+              exact={true}
+              render={(props: SignType): React.ReactElement => (
+                <Sign {...props} signUp={true} />
+              )}
+            />
+            <Redirect to="/" />
+          </Switch>
+        </>
+      )}
     </>
   ) : (
     <Loading />
