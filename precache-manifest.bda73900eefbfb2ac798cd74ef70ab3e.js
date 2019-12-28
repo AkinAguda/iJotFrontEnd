711f388b3fdde83cf017e189887c1b48831d92f5
Akin Aguda <lordtitan7@gmail.com>
2019-12-28 09:00:35 +0100

    Updates

---
+++ precache-manifest.bda73900eefbfb2ac798cd74ef70ab3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a17c97f9cf478eb9b726f1f789c6a56",
+    "url": "/iJotFrontEnd/index.html"
+  },
+  {
+    "revision": "1d3f8f8804559a71bba5",
+    "url": "/iJotFrontEnd/static/css/main.2345b250.chunk.css"
+  },
+  {
+    "revision": "b4d9b979889003f08dea",
+    "url": "/iJotFrontEnd/static/js/2.275421a6.chunk.js"
+  },
+  {
+    "revision": "1d3f8f8804559a71bba5",
+    "url": "/iJotFrontEnd/static/js/main.26c39b3e.chunk.js"
+  },
+  {
+    "revision": "fd4148f056b7a729f4f8",
+    "url": "/iJotFrontEnd/static/js/runtime~main.b0e1425b.js"
+  },
+  {
+    "revision": "46e5bec353400b567914a2db37d57ea0",
+    "url": "/iJotFrontEnd/static/media/Roll.46e5bec3.svg"
+  },
+  {
+    "revision": "d096301d2c8559228a2dde8e993403c4",
+    "url": "/iJotFrontEnd/static/media/check.d096301d.svg"
+  }
+]);
